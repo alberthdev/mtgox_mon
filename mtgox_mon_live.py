edd5f9c95d7bfe2fee1da725c67ac36a9f0fbaa9
Albert H <alberthrocks@users.noreply.github.com>
2013-12-01 15:51:14 -0500

    Updated license for mtgox_mon_live.py

--- mtgox_mon_live.py
+++ mtgox_mon_live.py
@@ -2,7 +2,19 @@
 # -*- coding: utf8 -*-
 # PyMtGoxMon v1.0 - Python-based monitor for MtGox BTC trading!
 # Copyright (C) 2013 Albert Huang.
-# Under GPL v3.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 import urllib2, json
 import datetime
 import time
