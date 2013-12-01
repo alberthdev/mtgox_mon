785e514a3a90a62323e6aa27f5581c3ad1fe07cb
Albert H <alberthrocks@users.noreply.github.com>
2013-12-01 15:52:12 -0500

    Updated license header for screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # PyMtGoxMon v1.0 - Python-based monitor for MtGox BTC trading!
 # Copyright (C) 2013 Albert Huang.
-# Under GPL v3.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 # 
 # Screenshot module for PyMtGoxMon. Requires pygtk to run!
 # screenshot.py
+# 
 import gtk.gdk
 
 def screenshot(file_name):
